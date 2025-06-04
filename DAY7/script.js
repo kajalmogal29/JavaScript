6a694906738105c12d67069458979be5805fb062
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-04 20:38:27 +0530

    add content in heading and paragraph

--- DAY7/script.js
+++ DAY7/script.js
@@ -5,5 +5,8 @@
 // document.wwriteln("hello")
 
 let heading = " <h1> Hello Easycode </h1>"
+let paragraph = "<p> This is a paragraph of text. </p>"
+
 document.writeln(heading)
+document.writeln(paragraph)
 
