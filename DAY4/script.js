e886af0b3dadb96a6657eca0f5ae29ecc42f045c
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-04 09:18:02 +0530

    learn console types

--- DAY4/script.js
+++ DAY4/script.js
@@ -1,6 +1,15 @@
 // let name = "welcome to javascript"
 // console.log(name)
 
-let a = 10
-let b = 20
-console.log(a+b)
+// let a = 10
+// let b = 20
+// console.log(a+b)
+
+// let tag_line = "Welcome to JavaScript"
+// console.info(tag_line)
+
+let rule = "Keep Silence"
+console.warn(rule)
+
+let abc = "something went wrong"
+console.error(abc)
