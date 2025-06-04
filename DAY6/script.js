6caa920eb59ea3a441633c918341cf68fd771371
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-04 20:04:51 +0530

    learn about ternary operators

--- DAY6/script.js
+++ DAY6/script.js
@@ -3,6 +3,20 @@
 // let result = age >= 18 ? "Able to vote" : "Not able to vote";
 // console.log(result)
 
-let abc = true
-let res = abc === true ? "congratulations" : "Never give up"
-console.log(res)
+// let abc = true
+// let res = abc === true ? "congratulations" : "Never give up"
+// console.log(res)
+
+let num = 12
+let result = num > 0 ? "Positive" : "Negative"
+console.log(result)
+
+let num = 10 
+let res = num / 2 ? "Even" : "Odd"
+console.log(res) 
+
+
+
+let role = "admin"
+let access = role === "admin" ? "Full access" : "Limited access"
+console.log(access)
