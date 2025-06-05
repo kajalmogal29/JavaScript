348a411425bcb5060b5f4ecbb483aa54a5f9c0fe
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 13:34:13 +0530

    studied while and do while loop

---
+++ DAYY14[LOOPS]/script.js
@@ -0,0 +1,30 @@
+let num = 10 
+
+while (num > 0) {
+    console.log(num)
+    num--
+}
+
+let num2 = 0
+while (num2 <= 10) {
+    console.log(num2)
+    num2++
+}
+
+number = 10
+
+while (number > 0) {
+    if (number % 2 == 0) {
+        console.log("even number")
+    }
+    else {
+        console.log("odd number")
+    }
+
+    number--
+}
+
+
+do {
+    console.log("hello")
+}while (false)
