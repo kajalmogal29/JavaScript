a8e8979accf60265fee20453cabf40302a0bbd3a
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 13:26:33 +0530

    learn about strings

--- DAYY13/script.js
+++ DAYY13/script.js
@@ -1,46 +1,93 @@
 
-let whatsUserData = {
+// let whatsUserData = {
      
-     name : "kajal",
+//      name : "kajal",
 
-     profile_picture : "myProfile.jpg",
+//      profile_picture : "myProfile.jpg",
 
-}
-
-
-
-  whatsUserData.name = "kajal"
-
-console.log(whatsUserData)
+// }
 
 
 
-let PhoneInfo = {
-     name :"Vivo",
-     prise:25000,
-     color:"diamond",
-}
+//   whatsUserData.name = "kajal"
+
+// console.log(whatsUserData)
+
+//************************************************************ */
+
+// let PhoneInfo = {
+//      name :"Vivo",
+//      prise:25000,
+//      color:"diamond",
+// }
+
+// PhoneInfo.isHaveCharger = false
+
+// delete PhoneInfo.prise
+
+// let ObjValues = Object.values(PhoneInfo)
+
+// console.log(ObjValues)
+//******************************************************************** */
+
+// let Doubt = {
+//      doubt : "how i insert new key in object"
+// }
+
+// Doubt.isDoubtSolve =  true
+
+// Doubt.answer = "i write directly Objname.newKey = value"
+
+// Doubt.date = "26/05/2025"
+
+// console.log(Doubt)
+
+//******************************************************************** */
+
+//******************************************************************* */
+//****************************************************************** */
+
+// START TO LEARN STRING IN JAVASCRIPT
+
+//***************************************************************** */
 
 
-PhoneInfo.isHaveCharger = false
 
-delete PhoneInfo.prise
+let name = "Kajal"
 
-let ObjValues = Object.values(PhoneInfo)
+let combinedData = "Kajal" + " " + "Mogal" + " " + 20
 
+console.log(combinedData)
 
-console.log(ObjValues)
+name.toLowerCase()
+console.log(name.toLowerCase())
 
-
-let Doubt = {
-     doubt : "how i insert new key in object"
-}
-
-Doubt.isDoubtSolve =  true
-
-Doubt.answer = "i write directly Objname.newKey = value"
-
-Doubt.date = "26/05/2025"
+name.toUpperCase()
+console.log(name.toUpperCase())
 
 
-console.log(Doubt)
+let quote =  "we Love Tto transform ideas into code" 
+
+console.log(quote)
+
+console.log(quote.length)
+
+console.log(quote.indexOf("Tto"))
+
+
+console.log(quote.slice(0, 10))
+
+console.log(quote.slice(0, 10).toUpperCase())
+
+console.log(quote.slice(0, 10).toLowerCase())
+
+console.log(quote.slice(0, 10).toUpperCase().length)
+
+console.log(quote.slice(0, 10).toLowerCase().length)
+
+console.log(quote.split(","))
+
+console.log(name.split(",")[1])
+console.log(name.split(",")[0])
+console.log(name.split(",")[2])
+console.log(name.split(",")[3])
