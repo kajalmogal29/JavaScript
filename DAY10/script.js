8dcf4e2ab7b975b8258525a281d0c4eacabad7eb
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 10:11:14 +0530

    learn switch cases

--- DAY10/script.js
+++ DAY10/script.js
@@ -1,3 +1,117 @@
- let marks = parseInt(prompt("enter your score in % : "));
+//  let marks = parseInt(prompt("enter your score in % : "));
 
 
+// if(marks >= 90){
+//    console.log("you get grade A");
+
+// }else if(marks >= 80){
+//      console.log("you get gread B");
+
+// }else if(marks >= 70){
+//      console.log("you get grade C");
+
+// }else{
+//      console.log("you get grade D");
+// }
+
+
+
+// let day = "sunday"
+
+// switch (day) {
+
+//     case "monday":
+//         console.log("today is monday");
+//          break;
+
+//     case "tuesday":
+
+//         console.log("today is tuesday");
+//          break;
+
+//     case "wednesday":
+
+//         console.log("today is wednesday");
+//         break;
+
+//     case "thursday":
+
+//         console.log("today is thursday");
+//         break;
+
+//     case "friday":
+
+//         console.log("today is friday");
+//         break;
+
+//     default : 
+//       console.log("i dont have these day ")
+
+// }
+
+
+
+// let ROLE = "admin";
+
+// if (ROLE === "admin") {
+
+//     console.log("you are admin")
+
+// } else if (ROLE == "user") {
+//     console.log("you are user")
+// } else {
+//     console.log("please login to get your role")
+// }
+
+
+
+// let num1 = 10
+// let num2 = 30
+
+// let operation = "a"
+
+// switch(operation){
+//     case "+" :
+//         console.log(num1 + num2);
+//         break;
+
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+
+//     case "*":
+//         console.log(num1 * num2);
+//         break;
+
+//     case "/" :
+//         console.log(num1 / num2);
+//         break;
+
+//     case "%" :
+//         console.log(num1 % num2);
+//         break;
+
+//     default:
+//          console.error(operation + " is not supported")
+// }
+
+
+
+// function checkPelindrome(str) {
+
+//     if(typeof str != "string"){
+
+//          return console.log("please enter string")
+//     }
+ 
+
+//     let splitString = str.split("").reverse().join("")
+
+//     if (str === splitString) {
+//         console.log("string is pelindrome")
+//     } else {
+//         console.log("string is not pelindrome")
+//     }
+// }
+
+// checkPelindrome("n");
