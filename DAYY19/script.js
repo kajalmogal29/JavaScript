c83cd45b84772c0d16e8c6e614cd27df07cb6dd2
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 17:20:24 +0530

    learn about Dom

---
+++ DAYY19/script.js
@@ -0,0 +1,42 @@
+// 1 Document.getElementById
+
+// let box = document.getElementById("box")
+// console.log(box)
+
+// let box1 = document.getElementById("box1")
+// box1.innerText = "Kajal"
+
+// let box2 = document.getElementById("box2").innerText = "Kajal"
+
+// box.innerHTML = <div id = "box1">Hello</div>
+
+// let box = document.getElementById("box2")
+// box.innerHTML = "<input type=number>"
+
+// let box = document.getElementById("box2")
+// box.style.backgroundColor = "red"
+
+// let box = document.getElementById("box")
+// box.style.margin = "auto"
+
+
+///////////////////////////////////////////////////////////
+
+// 2 document.getElementByClassName
+
+// let p = document.getElementsByClassName("para")
+// for(let para of p){
+//     para.innerHTML = "<h1>Great work</h1>"
+// }
+
+///////////////////////////////////////////////////////////
+
+// 3 document.getElementByTagName
+ // let p = document.getElementsByTagName("p")
+
+ /////////////////////////////////////////////////////////
+
+ // 4 document.querySelector("#")
+ // let box = document.querySelector("#box")
+//  let box = document.querySelector(".box")
+//  let box1 = document.querySelectorAll(".box")
