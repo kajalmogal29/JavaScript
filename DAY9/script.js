d2bbd9fa5bee1f423c0c5ab8c1bb478dda1e2f20
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 10:02:40 +0530

    if else practice

--- DAY9/script.js
+++ DAY9/script.js
@@ -3,4 +3,19 @@ if (num % 2 == 0) {
     console.log(num + " is even")
     } else {
         console.log(num + " is odd")
-        
+    }
+
+let num 2 = 50
+if (num > 0) {
+    console.log(num + " is positive")
+    } else { 
+        console.log(num + " is negative")
+    }
+
+
+let num = 223
+if (num / 3 == 0) {
+    console.log(num + " is divisible by 3")
+    } else {
+        console.log(num + " is not divisible by 3")
+    }
