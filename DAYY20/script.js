e7135b911282e025d58313b9156c0801cf0ea6dd
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 21:07:54 +0530

    Learn about querySelector

---
+++ DAYY20/script.js
@@ -0,0 +1,18 @@
+// let box = document.querySelector("#box")
+// console.log(box)
+
+let box = document.querySelector("#box")
+
+function clicked(){
+    console.log("element has been clicked")
+}
+box.addEventListener("click",clicked)
+
+/////////////////
+
+// get the element
+// add the event = click
+//change the button text
+
+let btn = document.querySelector("#btn")
+console.log()
