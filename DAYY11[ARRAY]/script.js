4d881f00f07a1adbfece72ea58339eb0a92dcef7
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 10:24:43 +0530

    syart to learn array

---
+++ DAYY11[ARRAY]/script.js
@@ -0,0 +1,105 @@
+
+// let cars = ['thar','ertica','swift','nexon']
+
+// console.log(cars[0])
+// console.log(cars[1])
+// console.log(cars[2])
+
+
+// let todos = ["hey wake up "] ;
+
+// todos.push("in");
+// todos.push(" the morning")
+// todos.push("8 O'clock ")
+
+// console.log(todos)
+
+
+
+// let colors = []
+
+// colors.push("red")
+// colors.push("orange")
+// colors.push("white")
+
+// console.log(colors)
+
+
+// let numbers =[]
+
+// numbers.push(100)
+// numbers.push(200)
+// numbers.push(300)
+
+// console.log(numbers)
+
+// numbers.unshift("50")
+// numbers.unshift("10")
+// numbers.unshift(0)
+
+// console.log(numbers)
+
+
+
+let cities = []
+
+cities.push("Pune")
+cities.push("Mumbai")
+cities.push("Delhi")
+cities.unshift("satara")
+
+console.log(cities)
+
+
+let myName =["V","R","U","S","H","A","L","I"];
+
+console.log(myName)
+
+let removedelm =myName.pop()
+
+console.log(removedelm)
+
+console.log(myName)
+
+
+
+let grade = ["A","B","C","D","E","F"]
+
+console.log(grade)
+
+grade.pop()
+
+grade.push("fail")
+grade.unshift("A+")
+
+
+console.log(grade)
+
+grade.shift()
+
+console.log(grade)
+
+
+let mobileCompanyes = ["apple"]
+
+
+mobileCompanyes.push("samsung");
+mobileCompanyes.push("realme")
+mobileCompanyes.unshift("redme")
+
+console.log(mobileCompanyes)
+
+mobileCompanyes.shift()
+mobileCompanyes.pop()
+
+
+console.log(mobileCompanyes[2])
+
+
+let requests = [""]  // it by default take 0
+
+if(requests.length <=0){
+     console.log("no one here")
+}else{
+    console.log("some reuests ")
+}
