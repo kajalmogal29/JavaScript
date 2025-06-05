b801d5a307a73351ac3f3e441998ac98aba88232
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-05 13:47:43 +0530

    learn functions

---
+++ DAYY17[FUNCTION]/script.js
@@ -0,0 +1,41 @@
+
+
+function sayHello() {
+console.log("Hello, World!");
+}
+
+
+sayHello();
+sayHello();
+sayHello();
+
+let name = function (name) {
+    return name
+}
+
+console.log(name("vrushali"))
+
+console.log(name);
+
+function add(a, b) {
+    return a + b
+}                   
+
+console.log(add(10, 20))
+
+function multify (a=5 ,b){
+    return a * b
+}
+
+console.log(multify(6,5))
+console.log(multify(10))
+
+let greet = function (name) {
+    return "Hello " + name
+}
+console.log(greet("vrushali"))
+console.log(greet("pune"))
+console.log(greet("maharashtra"))
+console.log(greet("india"))
+
+
