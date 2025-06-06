ed732052fc847b144f00f3199714bf7f7a1579b8
kajalmogal29 <kajalmogal29@gmail.com>
2025-06-06 11:38:48 +0530

    sidebar created

--- DAYY20/script.js
+++ DAYY20/script.js
@@ -1,12 +1,12 @@
 // let box = document.querySelector("#box")
 // console.log(box)
 
-let box = document.querySelector("#box")
+// let box = document.querySelector("#box")
 
-function clicked(){
-    console.log("element has been clicked")
-}
-box.addEventListener("click",clicked)
+// function clicked(){
+//     console.log("element has been clicked")
+// }
+// box.addEventListener("click",clicked)
 
 /////////////////
 
@@ -14,5 +14,40 @@ box.addEventListener("click",clicked)
 // add the event = click
 //change the button text
 
-let btn = document.querySelector("#btn")
-console.log()
+// let btn = document.querySelector("#btn")
+//  function changeBtnText(){
+//     btn.innerText = "hide"
+//  }
+//  btn.addEventListener("click",changeBtnText)
+
+
+// let btn = document.querySelector("#btn")
+//  function changeBtnText(){
+//     if (btn.innerText = "open"){
+//         btn.innerText = "hide"
+//         }else{
+//             btn.innerText = "open"
+//     }
+//  }
+//  btn.addEventListener("click",changeBtnText)
+
+// let btn = document.querySelector("#btn")
+// function changeBtnText(){
+//     console.log("double click")
+// }
+// btn.addEventListener("dblclick",changeBtnText) or btn.addEventListner("dbclick", function()=>{
+    //     console.log("double click")
+   //  } )
+
+
+let openBar = document.querySelector(".open")
+let menu = document.getElementById("menu")
+let closeBar = document.querySelector(".close")
+
+
+openBar.addEventListener("click", ()=>{
+    menu.style.right = "0"
+})
+closeBar.addEventListener("click", ()=>{
+menu.style.right = "-80vw"
+})
